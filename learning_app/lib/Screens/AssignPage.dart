fab9e1d3c7886d1dc30b71c63923219691435853
andrew-gan <andrewgan120410@gmail.com>
2020-07-08 12:30:59 -0400

    Creating and linking assignments page

---
+++ learning_app/lib/Screens/AssignPage.dart
@@ -0,0 +1,235 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'ProfilePage.dart';
+import '../CreateDB.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final FocusNode myFocusNode = FocusNode();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isInvalid = false;
+
+  @override
+  void initState() => super.initState();
+
+  @override
+  Widget build(BuildContext context) {
+    // insert test database
+    createDB();
+    
+    void onLoginPress() async {
+      final Future<Database> db = openDatabase(
+        join(await getDatabasesPath(), 'learningApp_database.db'),
+      );
+
+      final Database dbRef = await db; // capture a reference of the future type
+      final List<Map<String, dynamic>> res = await dbRef.query(
+        'accounts',
+        distinct: true,
+        where: 'email = \'' + emailController.text + '\' AND password = \'' +
+          passwordController.text + '\'',
+      );
+      List<AccountInfo> queryRes = List.generate(res.length, (i) {
+        return AccountInfo(
+          name: res[i]['name'],
+          email: res[i]['email'],
+          major: res[i]['major'],
+          year: res[i]['year'],
+          college: res[i]['college'],
+          regCourse: res[i]['regCourse'],
+        );
+      });
+
+      dbRef.close();
+      if (queryRes.length == 0)
+        setState(() => isInvalid = true);
+      else {
+        setState(() => isInvalid = false);
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ProfilePage(queryRes.first)
+        ));
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('Login'),
+      ),
+      body: Container(
+        color: Colors.blueGrey,
+        child: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                  height: 260.0,
+                  color: Colors.white,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 100.0),
+                        child: Stack(fit: StackFit.loose, children: <Widget>[
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                  width: 300.0,
+                                  height: 100.0,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.rectangle,
+                                    image: DecorationImage(
+                                      image: ExactAssetImage(
+                                          'assets/images/login.png'),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  )),
+                            ],
+                          ),
+                        ]),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Color(0xffFFFFFF),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 25.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text(
+                                    'Email ID',
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )),
+                      Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 2.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Flexible(
+                                child: TextField(
+                                  decoration: const InputDecoration(
+                                      hintText: "Enter email ID"),
+                                  controller: emailController,
+                                  enabled: true,
+                                ),
+                              ),
+                            ],
+                          )),
+                      Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 25.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text(
+                                    'Password',
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )),
+                      Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 2.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Flexible(
+                                child: TextField(
+                                  decoration: const InputDecoration(
+                                      hintText: "Enter password"),
+                                  controller: passwordController,
+                                  enabled: true,
+                                  obscureText: true,
+                                ),
+                              ),
+                            ],
+                          )),
+                      Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 35.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Visibility(
+                                child: Text(
+                                  'Incorrect email or password provided',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                replacement: Text(
+                                  '',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                                visible: isInvalid,
+                              ),
+                            ],
+                          )),
+                      Center(
+                          heightFactor: 2,
+                          child: OutlineButton(
+                            onPressed: onLoginPress,
+                            child: Text('LOGIN'),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0)),
+                          )),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
+    );
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    myFocusNode.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
--- learning_app/lib/Screens/ProfilePage.dart
+++ learning_app/lib/Screens/ProfilePage.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'CourseDetailsPage.dart';
+import 'CoursePage.dart';
 import '../CreateDB.dart';
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class ProfilePage extends StatefulWidget {
   final AccountInfo userinfo;
@@ -14,7 +17,7 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   final AccountInfo userinfo;
   List<String> regCourses;
-  final FocusNode myFocusNode = FocusNode();
+  List<AssignmentQuestionInfo> assignQInfo;
 
   _ProfilePageState(this.userinfo) {
     regCourses = this.userinfo.regCourse.split(',');
@@ -246,23 +249,48 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   Widget _buildRow(BuildContext context, int index) {
+    String courseID = regCourses[index];
     return ListTile(
       title: Text(
-        regCourses[index],
+        courseID,
       ),
       trailing: Icon(Icons.chevron_right),
       onTap: () {
+        _queryAssignmentInfo(courseID);
         Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => CourseDetailsPage(regCourses[index]),
+          builder: (context) => CoursePage(courseID, assignQInfo),
         ),);
       },
     );
   }
 
   @override
-  void dispose() {
-    // Clean up the controller when the Widget is disposed
-    myFocusNode.dispose();
-    super.dispose();
+  void dispose() => super.dispose();
+
+  void _queryAssignmentInfo(String courseID) async {
+    Future<Database> db = openDatabase(
+      join(await getDatabasesPath(), 'learningApp_database.db'),
+    );
+    Database dbRef = await db;
+
+    final List<Map<String, dynamic>> res = await dbRef.query(
+      'assignmentQuestions',
+      distinct: true,
+      where: 'courseID = \'' + courseID + '\'',
+      orderBy: 'dueDate',
+    );
+
+    List<AssignmentQuestionInfo> queryRes = List.generate(res.length, (i) {
+      return AssignmentQuestionInfo(
+        assignTitle: res[i]['assignTitle'],
+        courseID: res[i]['courseID'],
+        imageB64: res[i]['imageB64'],
+        content: res[i]['content'],
+        dueDate: res[i]['dueDate'],
+        instrEmail: res[i]['instrEmail'],
+      );
+    });
+
+    if(this.mounted == true) setState(() => assignQInfo = queryRes);
   }
 }
